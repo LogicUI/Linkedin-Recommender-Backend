dec581ebf77cb8de71e2eed00bf2d03922731267
John Wee <johnwee@Johns-Laptop.local>
2024-11-27 14:24:12 +0800

    Update vector_db with baii/bge-base-en-v1.5 embedded models and example user query

--- vector_db.py
+++ vector_db.py
@@ -1,29 +1,53 @@
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
-from llama_index.vector_stores.postgres import PGVectorStore
-from crypto import load_key , decrypt_folder 
-import logging
+from llama_index.core import VectorStoreIndex
+from llama_index.core import Settings
+from llama_index.readers.json import JSONReader
 from dotenv import load_dotenv
+from deep_seekllm import DeepSeekLLM 
+import logging
 import os 
+import sys
+import glob
 
-
-
-logging.basicConfig(level=logging.INFO)
-
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 load_dotenv()
 
-def create_llm():
-    os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_SECRET_KEY")
+def load_and_index_json(directory_path):
+    if not os.path.exists(directory_path):
+        logging.error(f"Folder {directory_path} does not exist")
+        sys.exit(1)
+        
+    reader = JSONReader( 
+        levels_back = 0,
+        collapse_length=None, 
+        ensure_ascii=False, 
+        is_jsonl=False, 
+        clean_json=True
+    )
+    
+    json_files = glob.glob(os.path.join(directory_path, "*.json"))
+
+    documents = []
+    for json_file in json_files:
+        documents.extend(reader.load_data(input_file=json_file, extra_info={}))
+        
+    index = VectorStoreIndex.from_documents(documents)
+    return index
+
+def query_index(index, query):
+    query_engine = index.as_query_engine()
+    response = query_engine.query(query)
+    return response
 
 
 if __name__ == "__main__":
-    create_llm()
-    key = load_key()
-    source_file = "./encrypted_files"
-    dest_file = "./decrypted_files"
-    if not os.path.exists(dest_file):
-        decrypt_folder(source_file, dest_file, key)
-        logging.info(f"Folder {dest_file} created")
-    else:
-        logging.info(f"Folder {dest_file} exist using exising file")
-
+    Settings.llm = DeepSeekLLM()
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+    Settings.embed_model = "local:BAAI/bge-base-en-v1.5"
+    index = load_and_index_json("./decrypted_files")
+    user_query = "Give a list of users who are software engineer in their occupations, in your response please return a list of eligible user json and their full json profiles"
+    response = query_index(index, user_query)
+    logging.info(f"Response: {response}")
+    
