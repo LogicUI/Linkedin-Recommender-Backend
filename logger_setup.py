5baf8729c523fa84f2f433ef79bcda3fa58bca7b
LogicUI <johnwee35@gmail.com>
2024-11-30 18:24:54 +0800

    edit logger to allow more functionality and add query time loger that tracks query time

--- logger_setup.py
+++ logger_setup.py
@@ -1,6 +1,6 @@
 import logging
 
-def setup_logger(name=None, level=logging.INFO):
+def setup_logger(name=None, level=logging.INFO, log_to_file=False, log_file="app.log"):
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
@@ -13,4 +13,28 @@ def setup_logger(name=None, level=logging.INFO):
 
         logger.addHandler(console_handler)
 
+        if log_to_file:
+            file_handler = logging.FileHandler(log_file)
+            file_handler.setLevel(level)
+
+            file_handler.setFormatter(formatter)
+
+            logger.addHandler(file_handler)
+
     return logger
+
+def setup_query_time_logger():
+    query_time_logger = logging.getLogger("query_time_logger")
+    query_time_logger.setLevel(logging.INFO)
+
+    query_time_handler = logging.FileHandler("query_time.log")
+    query_time_handler.setLevel(logging.INFO)
+
+    # Add a formatter to include timestamps
+    formatter = logging.Formatter("%(asctime)s - %(message)s")
+    query_time_handler.setFormatter(formatter)
+
+    # Add the handler to the logger
+    query_time_logger.addHandler(query_time_handler)
+
+    return query_time_logger
